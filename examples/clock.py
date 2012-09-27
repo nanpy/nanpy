764633328d26643e376584efddf7ad31957b9050
4ndreaSt4gi <stagi.andrea@gmail.com>
2012-09-27 10:09:16 +0200

    Clock looks nice now

--- examples/clock.py
+++ examples/clock.py
@@ -13,7 +13,7 @@ from datetime import datetime
 ntp_client = ntplib.NTPClient()
 response = ntp_client.request('europe.pool.ntp.org', version=3)
 
-time = response.tx_time
+time = int(response.tx_time)
 
 lcd = Lcd([7, 8, 9, 10, 11, 12], [16, 2])
 
@@ -21,6 +21,9 @@ while (1):
     lcd.setCursor(0, 0)
     lcd.printString((datetime.fromtimestamp(time)).strftime('%Y-%m-%d'))
     lcd.setCursor(0, 1)
-    lcd.printString((datetime.fromtimestamp(time)).strftime('%H:%M'))
+    if time % 2:
+        lcd.printString((datetime.fromtimestamp(time)).strftime('%H:%M'))
+    else:
+        lcd.printString((datetime.fromtimestamp(time)).strftime('%H %M'))
     Arduino.delay(1000)
     time += 1
