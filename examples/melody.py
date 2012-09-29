b7dbc118578c760b90adfa26266cb390da070a76
Andrea Stagi <stagi.andrea@gmail.com>
2012-09-29 17:33:58 +0200

    Minfixes to melody example

--- examples/melody.py
+++ examples/melody.py
@@ -10,8 +10,8 @@ melody = [[Tone.NOTE_C4, 4], [Tone.NOTE_G3, 8], [Tone.NOTE_G3, 8],
             [Tone.NOTE_A3, 4], [Tone.NOTE_G3, 4], [0, 4], 
             [Tone.NOTE_B3, 4], [Tone.NOTE_C4, 4]]
 
-tone = Tone(8)
+tone = Tone(13)
 
 for note in melody:
-    tone.play(note[0] , 1000/note[1]);
+    tone.play(note[0] , 1000/note[1])
 
