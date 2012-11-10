e04b23d52e1001cb2cb745c0d6cd0abfedf2f6f0
4ndreaSt4gi <stagi.andrea@gmail.com>
2012-11-10 21:49:43 +0100

    Back to use Serial

--- firmware/ComChannel.h
+++ firmware/ComChannel.h
@@ -13,62 +13,34 @@ class ComChannel {
     public:
 
         static bool available() {
-            #if BOARD == 2
-            if (Serial1.available() > 0)
-            #else
             if (Serial.available() > 0)
-            #endif
                 return true;
             else
                 return false;
         }
 
         static void connect() {
-            #if BOARD == 2
-            Serial1.begin(BAUDRATE);
-            #else
             Serial.begin(BAUDRATE);
-            #endif
         }
 
         static void println(String& val) {
-            #if BOARD == 2
-            Serial1.println(val);
-            #else
             Serial.println(val);
-            #endif
         }
 
         static void println(const char* val) {
-            #if BOARD == 2
-            Serial1.println(val);
-            #else
             Serial.println(val);
-            #endif
         }
 
         static void println(int val) {
-            #if BOARD == 2
-            Serial1.println(val);
-            #else
             Serial.println(val);
-            #endif
         }
 
         static void println(float val) {
-            #if BOARD == 2
-            Serial1.println(val);
-            #else
             Serial.println(val);
-            #endif
         }
 
         static void println(double val) {
-            #if BOARD == 2
-            Serial1.println(val);
-            #else
             Serial.println(val);
-            #endif
         }
 
 };
