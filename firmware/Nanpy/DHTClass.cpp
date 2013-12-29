f110873f6d258ee8b57a583d11b78599199f81ff
Laurent Desarmes <laurent+chef@u-picardie.fr>
2013-12-29 14:02:57 +0100

    begin is called when instantiating the class

--- firmware/Nanpy/DHTClass.cpp
+++ firmware/Nanpy/DHTClass.cpp
@@ -4,26 +4,28 @@
 #include <stdlib.h>
 
 void nanpy::DHTClass::elaborate( MethodDescriptor* m ) {
-    if (strcmp(m->getClass(), "DHT") == 0) {
+  if (strcmp(m->getClass(), "DHT") == 0) {
 
-        ObjectsManager<DHT>::elaborate(m);
+    ObjectsManager<DHT>::elaborate(m);
 
-        if (strcmp(m->getName(),"new") == 0) {
-	  v.insert(new DHT (m->getInt(0), m->getInt(1), m->getInt(2)));
-            m->returns(v.getLastIndex());
-        }
-
-        if (strcmp(m->getName(), "begin") == 0) {
-            v[m->getObjectId()]->begin();
-            m->returns(0);
-        }
-
-        if (strcmp(m->getName(), "readHumidity") == 0) {
-	  m->returns(v[m->getObjectId()]->readHumidity());
-        }
-
-        if (strcmp(m->getName(), "readTemperature") == 0) {
-	  m->returns(v[m->getObjectId()]->readTemperature(m->getBool(0)));
-        }
+    if (strcmp(m->getName(),"new") == 0) {
+      DHT* dht;
+      if (m->getNArgs() == 3) {
+	dht = new DHT (m->getInt(0), m->getInt(1), m->getInt(2));
+      } else {
+	dht = new DHT (m->getInt(0), m->getInt(1));
+      }
+      dht->begin();
+      v.insert(dht);
+      m->returns(v.getLastIndex());
     }
+
+    if (strcmp(m->getName(), "readHumidity") == 0) {
+      m->returns(v[m->getObjectId()]->readHumidity());
+    }
+
+    if (strcmp(m->getName(), "readTemperature") == 0) {
+      m->returns(v[m->getObjectId()]->readTemperature(m->getBool(0)));
+    }
+  }
 };
