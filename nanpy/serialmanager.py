3269af206dfbce72a0251236ef639403e26323b1
ponty <ponty@home>
2013-12-29 20:49:44 +0100

    SerialManager: 2 sec sleep, autodetection skippable, lazy open (#10)
    - configurable (default:2 sec) sleep after connect
    - port autodetection can be skipped
    - connect only if necessary (lazy open)

--- nanpy/serialmanager.py
+++ nanpy/serialmanager.py
@@ -47,26 +47,31 @@ class NoneSerialManager(object):
 
 
 class SerialManager(object):
+    _serial = None
 
-    def __init__(self, device=None, baudrate=DEFAULT_BAUDRATE):
+    def __init__(self, device=None, baudrate=DEFAULT_BAUDRATE, sleep_after_connect=2):
+        self.device = device
         self.baudrate = baudrate
-        if device:
-            self.connect(device)
-        else:
-            # auto detection?
-            available_ports = _auto_detect_serial_unix()
-            try:
-                self.connect(available_ports[0])
-            except:
-                print("Error trying to connect to Arduino")
-                self._serial = NoneSerialManager()
+        self.sleep_after_connect = sleep_after_connect
 
-    def connect(self, device):
-        self._serial = serial.Serial(device, self.baudrate, timeout=1)
-#         time.sleep(2)
-        self.flush_input()
+    def connect(self, device=None):
+        if device:
+            self.device = device
+
+        if not self.device:
+            ports = _auto_detect_serial_unix()
+            assert len(ports), "No port was set, and no port was found!"
+            self.device = ports[0]
+        log.debug('opening port:%s [%s baud]' % (self.device, self.baudrate))
+        assert self.device
+        self._serial = serial.Serial(self.device, self.baudrate, timeout=1)
+        if self.sleep_after_connect:
+            time.sleep(self.sleep_after_connect)
+        self._serial.flushInput()
 
     def write(self, value):
+        if not self._serial:
+            self.connect()
         log.debug('sending:%s' % repr(value))
         if PY3:
             self._serial.write(bytes(value, 'latin-1'))
@@ -74,6 +79,8 @@ class SerialManager(object):
             self._serial.write(value)
 
     def readline(self):
+        if not self._serial:
+            self.connect()
         s = self._serial.readline()
         log.debug('received:%s' % repr(s))
         s = s.decode()
@@ -83,32 +90,10 @@ class SerialManager(object):
 
     def flush_input(self):
         '''Flush input buffer, discarding all it's contents.'''
+        if not self._serial:
+            self.connect()
         self._serial.flushInput()
 
 
-# TODO: remove it
-# created only for compaibility
-class SerialManagerCompatibility(object):
-    def __init__(self, device=None, baudrate=DEFAULT_BAUDRATE):
-        self.baudrate = baudrate
-        self.device = device
-
-    @property
-    @memoized
-    def manager(self):
-        return SerialManager(self.device, self.baudrate)
-
-    def connect(self, device):
-        return self.manager.connect(device)
-
-    def write(self, value):
-        return self.manager.write(value)
-
-    def readline(self):
-        return self.manager.readline()
-
-    def flush_input(self):
-        return self.manager.flush_input()
-
-serial_manager = SerialManagerCompatibility()
+serial_manager = SerialManager()
 
