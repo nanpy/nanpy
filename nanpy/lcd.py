ec5f79bbe00745eaecbc13467864f8bc9b090144
ponty <ponty@home>
2013-12-29 18:02:35 +0100

    more doc

--- nanpy/lcd.py
+++ nanpy/lcd.py
@@ -3,8 +3,12 @@ from nanpy.arduinoboard import arduinoobjectmethod
 
 class Lcd(ArduinoObject):
 
-    def __init__(self, pins, begin):
-        ArduinoObject.__init__(self)
+    def __init__(self, pins, begin, connection=None):
+        '''
+        :param pins: [rs, enable, d4, d5, d6, d7]
+        :param begin: [cols, rows]
+        '''
+        ArduinoObject.__init__(self, connection=connection)
         self.id = self.call('new', pins, begin)
 
     @arduinoobjectmethod
