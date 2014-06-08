0d158596edec70edfabedd16efd12d0de0c61531
Andrea Stagi <stagi.andrea@gmail.com>
2014-06-08 03:17:28 +0200

    Added setResolution to DallasTemperature sample, request the address before cycle

--- nanpy/examples/dallastemperature.py
+++ nanpy/examples/dallastemperature.py
@@ -8,10 +8,11 @@ from nanpy import DallasTemperature
 
 sensors = DallasTemperature(5)
 
+print("There are %d devices connected on pin %d" % (sensors.getDeviceCount(), sensors.pin))
+addr = sensors.getAddress(0)
+sensors.setResolution(9)
 while True:
-    print("There are %d devices connected on pin %d" % (sensors.getDeviceCount(), sensors.pin))
     sensors.requestTemperatures(0)
-    addr = sensors.getAddress(0)
     temp = sensors.getTempC(addr)
     print("The temperature, in Celsius degrees is %0.2f" % temp)
     print("Let's convert it in Fahrenheit degrees: %0.2f" % DallasTemperature.toFahrenheit(temp))
