4ba2be6a30c38c8609db1d64abebacfd24f815da
Andrea Stagi <stagi.andrea@gmail.com>
2014-10-02 09:55:27 +0200

    Version 0.9.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="nanpy",
-      version="0.9.1",
+      version="0.9.2",
       description="Use your Arduino board with Python",
       license="MIT",
       author="Andrea Stagi",
