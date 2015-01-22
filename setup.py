458d7bf190629e02d14c20c679d687f9befbf2ba
ponty <ponty@home>
2015-01-22 16:07:15 +0100

    adding classifiers

--- setup.py
+++ setup.py
@@ -2,6 +2,22 @@
 
 from setuptools import setup, find_packages
 
+classifiers = [
+# Get more strings from
+# http://pypi.python.org/pypi?%3Aaction=list_classifiers
+"License :: OSI Approved :: MIT License",
+"Natural Language :: English",
+"Operating System :: OS Independent",
+"Programming Language :: Python",
+"Programming Language :: Python :: 2",
+"Programming Language :: Python :: 2.6",
+"Programming Language :: Python :: 2.7",
+"Programming Language :: Python :: 3",
+"Programming Language :: Python :: 3.1",
+"Programming Language :: Python :: 3.2",
+"Programming Language :: Python :: 3.3",
+]
+
 setup(name="nanpy",
       version="0.9.2",
       description="Use your Arduino board with Python",
@@ -14,4 +30,5 @@ setup(name="nanpy",
       install_requires=[
         "pyserial",
       ],
+      classifiers=classifiers,
       zip_safe = True)
