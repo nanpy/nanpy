ccf0ed4f24e2ba414eaed89f391120a8dbab0dcb
ponty <ponty@home>
2015-02-04 15:31:29 +0100

    drop support for py31, add for py34

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ classifiers = [
 "Programming Language :: Python :: 2.6",
 "Programming Language :: Python :: 2.7",
 "Programming Language :: Python :: 3",
-"Programming Language :: Python :: 3.1",
 "Programming Language :: Python :: 3.2",
 "Programming Language :: Python :: 3.3",
+"Programming Language :: Python :: 3.4",
 ]
 
 setup(name="nanpy",
